bcd570762d46f84678f79084ad4edc82745b5bcc
Ben Sullivan <bensully513@gmail.com>
2018-04-24 14:43:10 -0600

    added queries

---
+++ knexfile.js
@@ -0,0 +1,14 @@
+require('dotenv').load()
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost:/coffee'
+
+  },
+
+  production: {
+    client: 'pg',
+    connection: 'connection: `${process.env.DATABASE_URL}?ssl=true`'
+  }
+}
